017203ceaaa9f8c1c763478ccdc7ef5548760098
mayn <1416279053@qq.com>
2019-11-14 13:57:12 +0800

    自动化测试python2

--- brother_wu_19/test/2test.py
+++ brother_wu_19/test/2test.py
@@ -1,20 +1,28 @@
 
 
 taiwei = [
-{"name":"俊杰","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"老葛","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"付名","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"李超","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"产品","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"开发","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
-{"name":"测试","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"]},
+{"name":"俊杰","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":"20"},
+{"name":"老葛","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":"20"},
+{"name":"付名","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":20},
+{"name":"李超","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":20},
+{"name":"李超","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":21},
+{"name":"开发","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":25},
+{"name":"测试","sex":"man","city":"shenzhen","gongsi":["taiwei","yuheng"],"age":20},
 ]
 
 
 for i in range(0,len(taiwei)):
     person = taiwei[i]
-    if person["name"] == "李超":
-        print(taiwei[i])
-        break
+    # if person["name"] == "李超":
+    #     print(taiwei[i])
+    #
+    # else:
+    #    pass
+    # if person["name"]=="李超" and person["age"]>20:
+    #     print(person["name"])
+    # else:
+    #     print("________NONE______")
+    if person["name"] == "李超" or int(person["age"]) > 20:
+     print(taiwei[i])
     else:
-       pass
+     print("________NONE______")
