1be166c0fc82ac9fd11c45cde0765fe4ba85f20f
mayn <1416279053@qq.com>
2019-11-15 18:02:40 +0800

    第三节，函数的定义20191115

--- brother_wu_19/test/3test.py
+++ brother_wu_19/test/3test.py
@@ -13,13 +13,26 @@ def 函数名称（）：
         函数名称（）
     有函数：
         函数名称（实参值）
+丰富你的函数-返回值
+语法:
+    :return[变量]
+def 函数的名称（参数）：
+    函数体（实现功能的代码段）
+    :return 变量（没有变量，返回None）
+
+1.返回值可以是任何类型的变量
+2.返回值也可以是表达式
+3，可以返回一个/多个变量，可以用逗号隔开或者元祖
+4，函数体执行过程中，遇到return意味着函数调用结束
+5.函数中没有return关键字，默认返回None
+6.使用较多，返回的值可以用来传递给其他函数
 '''
-
-def san():
-    print("遮风挡雨遮阳")
-
-san()
-san()
+#
+# def san():
+#     print("遮风挡雨遮阳")
+#
+# san()
+# san()
 
 def get_money_from_ATM(bank_id,passwd,money):
     #验证你的银行卡是否有效，是否有余额
@@ -28,6 +41,7 @@ def get_money_from_ATM(bank_id,passwd,money):
     #以上符合的情况下，吐出人民币给你，完了再吐出你的银行卡
     print("******")
     print(bank_id,passwd,money)
+    return bank_id,money
 
 
 icbc_card_id ="1111111111"
@@ -35,4 +49,5 @@ passwd="123456"
 money=2000
 
 
-get_money_from_ATM(icbc_card_id,passwd,money)
+ccc,mon=get_money_from_ATM(icbc_card_id,passwd,money)
+print(ccc,money)
