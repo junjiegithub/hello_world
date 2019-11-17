f9f7adba89f2faddd3026e5bc57db4ff70037bc8
mayn <1416279053@qq.com>
2019-11-17 22:58:10 +0800

    selenium WEB 自动化测试

---
+++ brother_wu_19/test/python_selenium_WEB.py
@@ -0,0 +1,21 @@
+'''
+
+python+selenium-web自动化测试（功能测试）
+web自动化测试
+1.web网页-系统对象
+2.在浏览器中访问
+3.通过浏览器与web网页进行交互（操作页面，输入，网页输出）
+目标：代码代替人-驱动-完成在浏览器上操作页面中的元素。
+
+web自动化-环境安装
+1.安装浏览器-Google,Firefox,IE....
+    windows平台:双击exe文件运行即可。
+2.安装浏览器驱动-chromedriver.exe,geckodriver.exeiedriver.exe....
+    拷贝XXdriver.exe到python安装目录下
+3.安装selenium
+    cmd命令行运行:pip install -U selenium
+
+4.框架--调用款阿静
+
+
+'''
