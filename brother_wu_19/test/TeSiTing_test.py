eca8babbb563d2d78549a9cf6da899aae5ab2763
mayn <1416279053@qq.com>
2019-12-10 18:31:07 +0800

    Selenium启动Chrome时，加载用户配置文件

--- brother_wu_19/test/TeSiTing_test.py
+++ brother_wu_19/test/TeSiTing_test.py
@@ -17,10 +17,35 @@
     （搞清楚自己要做什么）
     #selenium打开浏览器
     输入网址
-
+    (问题1：driver路径配置
+     问题2：加载太慢，或者使用缓存--user-dir-data=)
 """
 #coding = utf-8
+# selenium打开浏览器
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+#https://www.cnblogs.com/dieyaxianju/p/8540740.html
+#https://blog.csdn.net/qq_42735170/article/details/90712556,(unknown error: DevToolsActivePort file doesn't exist)
+'''
+#https://www.cnblogs.com/dieyaxianju/p/8540740.html
+Selenium启动Chrome时，加载用户配置文件
+
+#https://blog.csdn.net/qq_42735170/article/details/90712556,
+(unknown error: DevToolsActivePort file doesn't exist)
+在判断自己代码没错之后中与发现了问题所在
+这个功能只能第一次打开的浏览器中使用，第一个不结束，第二个再使用就会出错
+，最后的解决办法就是关闭所有的浏览器 然后从新启动脚本
+ 
+'''
+#添加用户文件配置
+option =webdriver.ChromeOptions()
+option.add_argument('--user-data-dir=C:\\Users\\mayn\\AppData\\Local\\Google\\Chrome\\User Data')
+
+# # selenium打开浏览器,executable_path='../Lib'
+driver=webdriver.Chrome(chrome_options=option)
+# driver=webdriver.Chrome()
+#打开网站
+driver.get('http://112.74.191.10:8000/')
+# driver.get('http://sitold.uhuasuan.com/')
 
 
-driver=webdriver.Chrome()
