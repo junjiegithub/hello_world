e461e9aeb34127d53d90f699e975aa930cb92010
mayn <1416279053@qq.com>
2019-12-23 18:46:39 +0800

    html.html

--- brother_wu_19/11/exercise24.py
+++ brother_wu_19/11/exercise24.py
@@ -45,3 +45,5 @@ for+range()
 # r = range(1,11,2)
 # for i in r:
 #     print(i)
+
+print('\n'.join([''.join([('Love'[(x-y) % len('Love')] if ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3 <= 0else' ') for x in range(-30, 30)]) for y in range(30, -30, -1)]))
--- brother_wu_19/test/TeSiTing_GitPythonJenkins.py
+++ brother_wu_19/test/TeSiTing_GitPythonJenkins.py
@@ -1,6 +1,6 @@
 '''
 Git+Python+Jenkins持续集成架构体系实战
-
+112.74.191.10/inter/register.html
 .详细内容：
     1.Jenkins平台安装和使用
     2.持续集成
@@ -11,4 +11,24 @@ Git+Python+Jenkins持续集成架构体系实战
 1.软件开发
     #v模型，w模型
     Jenkins
+    DevOps自动化实现
+    环境
+Jenkins服务器-开发部分(springboot)-源码-svn，git-编译MAVEN，ANT
+打包，jar包，war包，exe，release--运维部署，Linux，Shell
+
+文档安装
+1.文档讲解
+2.基本使用：
+        登录
+        创建项目
+        Hudson，TeamCity等
+1.下载Jenkinswar，部署到Tomcat下面。在浏览器中访问Jenkins目录。
+https://jenkins.io/download
+2.等到网页初始化成功，直到出现下面的界面：http://localhost:8080/jenkins
+
+    1.DevOps自动化实现
+        环境：jdk1.8，tomcat8.5
+        https://tomcat.apache.org/download-90.cgi  tomcat
+
+    2.持续集成实现
 '''
