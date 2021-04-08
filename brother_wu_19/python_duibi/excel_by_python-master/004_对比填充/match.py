983a26805e0196d94de85cb02d1ed94f71e470c7
Administrator <1416279053@qq.com>
2021-04-09 00:31:33 +0800

    #python自动化测试-python数据对比

--- brother_wu_19/python_duibi/excel_by_python-master/004_对比填充/match.py
+++ brother_wu_19/python_duibi/excel_by_python-master/004_对比填充/match.py
@@ -23,19 +23,22 @@
 
 import pandas as pd
 import os
-
+import xlwt
 
 # 读取表
-df_my_books = pd.DataFrame(pd.read_excel('excels/作品信息表.xlsx', sheet_name='Sheet1'))
+# df_my_books = pd.DataFrame(pd.read_excel('excels/作品信息表.xlsx', sheet_name='Sheet1'))
+#
+# df_my_author = pd.DataFrame(pd.read_excel('excels/作者表.xlsx', sheet_name='Sheet1'))
+df_my_books = pd.DataFrame(pd.read_excel('excels/充值工单005.xlsx', sheet_name='Sheet1'))
 
-df_my_author = pd.DataFrame(pd.read_excel('excels/作者表.xlsx', sheet_name='Sheet1'))
+df_my_author = pd.DataFrame(pd.read_excel('excels/充值工单.xlsx', sheet_name='Sheet1'))
 
-# 新建空列表，把想要的那一些行放进去列表里。
+# 新建空列表，pip3 install -i https://pypi.doubanio.com/simple/
 my_list = []
 
 for i in range(len(df_my_author)):
     for j in range(len(df_my_books)):
-        if str(df_my_books.iloc[j]['Author']) == str(df_my_author.iloc[i]['Author']):
+        if str(df_my_books.iloc[j]['工单id']) == str(df_my_author.iloc[i]['工单id']):
             # 把想要的那一行放进去列表里：这行的数据就是df_my_books.iloc[j]
             my_list.append(df_my_books.iloc[j])
 
@@ -43,7 +46,7 @@ for i in range(len(df_my_author)):
 df_same_df = pd.DataFrame(my_list)
 
 # 结果输出到result.xls
-filepath = 'result.xls'
+filepath = 'result.xlsx'
 writer = pd.ExcelWriter(filepath)
 df_same_df.to_excel(excel_writer=writer, index=False, sheet_name='Sheet1')
 writer.save()
